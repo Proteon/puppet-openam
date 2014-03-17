b0758a9f426064510af89d651226f844311d7d5c
Sander Bilo <sbilo@github.com>
2014-03-17 10:47:20 +0100

    Adding manifests

---
+++ manifests/instance.pp
@@ -0,0 +1,16 @@
+define openam::instance (
+  $version,
+  $instance = $name,
+) {
+  if (!defined(Tomcat::Instance[$instance])) {
+    tomcat::instance { $instance: }
+  }
+
+  tomcat::webapp::maven { 'openam':
+    instance   => $instance,
+    artifactid => 'openam-server',
+    groupid    => 'org.forgerock.openam',
+    version    => $version,
+    repos      => 'http://maven.forgerock.org/repo/releases',
+  }
+}
